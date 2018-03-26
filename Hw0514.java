339280461c787ccdc4802005d94797f4b0a4d673
Chris <37694710+kocoolcool@users.noreply.github.com>
2018-03-27 00:08:06 +0800

    loop break&continue

---
+++ Hw0514.java
@@ -0,0 +1,17 @@
+package Chris.Chapter05;
+
+public class Hw0514 {
+
+	public static void main(String[] args) {
+		int count = 0, l = 3500;
+		for (l = 3500; l > 0; l = l / 2) {
+			count++;
+			if (l <3) {
+				break;
+			}
+		}
+		System.out.printf("L=%d公尺  Time=%d天", l, count);
+
+	}
+
+}
